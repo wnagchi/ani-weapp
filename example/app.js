a1442110deaba96f2947f0f77656bf99a960a8fe
wnagchi <1305bcd@gmail.com>
2021-08-11 22:26:14 +0800

    新增了监听页面栈功能

--- example/app.js
+++ example/app.js
@@ -1,9 +1,10 @@
 // app.js
 import Ani from './ani'
-
 App({
   Ani:new Ani(),
   onLaunch() {
-
+      this.Ani.listenPage(function(page,routerName){
+          console.log(page,routerName);
+      })
   },
 })
